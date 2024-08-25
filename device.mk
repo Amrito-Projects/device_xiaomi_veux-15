9a250c970188096f64d1e45f6a0316d38ead31f7
kamikaonashi <thorschi93@gmx.at>
2024-08-25 08:58:28 +0200

    veux: switch back to lineage libperfmgr

--- device.mk
+++ device.mk
@@ -414,7 +414,7 @@ endif
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
@@ -511,6 +511,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
