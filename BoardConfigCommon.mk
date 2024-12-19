xiaomi-sm6375-devs/android_device_xiaomi_sm6375-common

sm6375-common: Import product props
 - V13.0.8.0.RKCMIXM

Change-Id: I54c4a00971a42750a4b0809913250f72b0b7c167

BoardConfigCommon.mk
@@ -168,6 +168,7 @@ TARGET_BOARD_PLATFORM := holi
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop

Amrito-Projects/device_xiaomi_veux-15

BoardConfigCommon.mk
@@ -168,6 +168,7 @@ TARGET_BOARD_PLATFORM := holi
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop